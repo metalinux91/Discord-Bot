a8cea8d856f07c87fdd0ea9747201f9ac3211ca0
João Coelho <coelho_joao_rafael@protonmail.com>
2020-07-04 17:53:45 -0400

    Include logs for most important events

--- bot.js
+++ bot.js
@@ -5,7 +5,9 @@ const Discord = require('discord.js');
 
 moment.tz.add('Europe/Lisbon|LMT WET WEST WEMT CET CEST|A.J 0 -10 -20 -10 -20|012121212121212121212121212121212121212121212321232123212321212121212121212121212121212121212121214121212121212121212121212121212124545454212121212121212121212121212121212121212121212121212121212121212121212121212121212121|-2le00 aPX0 Sp0 LX0 1vc0 Tc0 1uM0 SM0 1vc0 Tc0 1vc0 SM0 1vc0 6600 1co0 3E00 17c0 1fA0 1a00 1io0 1a00 1io0 17c0 3I00 17c0 1cM0 1cM0 3Fc0 1cM0 1a00 1fA0 1io0 17c0 1cM0 1cM0 1a00 1fA0 1io0 1qM0 Dc0 1tA0 1cM0 1dc0 1400 gL0 IM0 s10 U00 dX0 Rc0 pd0 Rc0 gL0 Oo0 pd0 Rc0 gL0 Oo0 pd0 14o0 1cM0 1cP0 1cM0 1cM0 1cM0 1cM0 1cM0 3Co0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 pvy0 1cM0 1cM0 1fA0 1cM0 1cM0 1cN0 1cL0 1cN0 1cM0 1cM0 1cM0 1cM0 1cN0 1cL0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|27e5');
 
-const client = new Discord.Client();
+function logger(msg) {
+  return `${moment().format('YYYY-MM-DDTHH:MM')} ${msg}`;
+}
 
 function getRandomDate() {
   const now = moment.tz(process.env.SERVER_TIMEZONE);
@@ -22,16 +24,20 @@ function getRandomDate() {
   const min = Math.ceil(startDate.valueOf());
   const max = Math.floor(endDate.valueOf());
 
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const nextTick = Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(logger(`Next tick will take place at ${moment(nextTick).format('YYYY-MM-DDTHH:mm')}`));
+
+  return nextTick;
 }
 
-const morde = require('./champions/morde')(client, getRandomDate);
+const client = new Discord.Client();
+const morde = require('./champions/morde')(client, getRandomDate, logger);
 const msgHandler = require('./msgHandler')(morde, getRandomDate);
 
 global.mordeTimeout = null;
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+  console.log(logger(`Logged in as ${client.user.tag}`));
 
   // Set timeout for Mordekaiser on bot start
   global.mordeTimeout = setTimeout(() => morde.realmOfDeath(), getRandomDate() - (new Date()).getTime());
--- champions/morde.js
+++ champions/morde.js
@@ -6,7 +6,7 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; // The maximum is inclusive and the minimum is inclusive
 }
 
-module.exports = function (client, getRandomDate) { // eslint-disable-line func-names
+module.exports = function (client, getRandomDate, logger) { // eslint-disable-line func-names
   function realmOfDeath() {
     // find the voice channel with most users on it
     const server = client.guilds.get(process.env.SERVER_ID); // only act on server specified in .env
@@ -31,26 +31,37 @@ module.exports = function (client, getRandomDate) { // eslint-disable-line func-
     // Get a random member
     // Turn map of members to array, filter out bots, get random index
     const randomMember = ([...mostFilledChannel.members.values()].filter((e) => !e.user.bot))[getRandom(0, mostFilledChannel.members.size - 1)];
+    console.log(logger(`Gonna be moving ${randomMember.user.username}`));
 
     mostFilledChannel.join() // connect to channel
       .then((connection) => new Promise((resolve) => {
         // play random voice clip
         const voices = fs.readdirSync(process.env.TAUNT_VOICES_DIR);
+        console.log(logger('Getting ready to play voice clip...'));
         const dispatcher = connection.playFile(`${process.env.TAUNT_VOICES_DIR}/${(voices.splice(getRandom(0, voices.length - 1), 1))[0]}`);
 
         // on finishing the clip, send previously selected random member to "Shadow Realms" channel specified in .env
-        dispatcher.on('end', () => resolve(randomMember.setVoiceChannel(process.env.SHADOW_REALMS_CHANNEL_ID)));
+        dispatcher.on('end', () => {
+          console.log(logger('Played voice clip. About to move member...'));
+          resolve(randomMember.setVoiceChannel(process.env.SHADOW_REALMS_CHANNEL_ID));
+        });
       }))
       .then(() => {
         // join the "Shadow Realms channel
         const shadowRealmsChannel = client.channels.get(process.env.SHADOW_REALMS_CHANNEL_ID);
+        console.log(logger('Moved member. Moving to his channel...'));
         return shadowRealmsChannel.join();
       })
       .then((connection) => {
         // play a random clip and disconnect
         const voices = fs.readdirSync(process.env.SHADOW_REALMS_VOICES_DIR);
         const dispatcher = connection.playFile(`${process.env.SHADOW_REALMS_VOICES_DIR}/${(voices.splice(getRandom(0, voices.length - 1), 1))[0]}`);
-        dispatcher.on('end', () => connection.disconnect());
+        console.log(logger('About to play voice clip...'));
+        dispatcher.on('end', () => {
+          console.log(logger('Played voice clip'));
+          connection.disconnect();
+          console.log(logger('Disconnected'));
+        });
 
         // set new timeout
         global.mordeTimeout = setTimeout(() => realmOfDeath(), getRandomDate() - (new Date()).getTime());
